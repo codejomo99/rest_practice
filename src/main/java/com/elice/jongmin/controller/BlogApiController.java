87a7465d2dea39ef017809b50b116954695c16e5
Jong-Min <codejomo99@gmail.com>
2024-05-09 13:21:41 +0900

    feat : Article Read

--- src/main/java/com/elice/jongmin/controller/BlogApiController.java
+++ src/main/java/com/elice/jongmin/controller/BlogApiController.java
@@ -2,12 +2,16 @@ package com.elice.jongmin.controller;
 
 import com.elice.jongmin.domain.Article;
 import com.elice.jongmin.dto.ArticleRequest;
+import com.elice.jongmin.dto.ArticleResponse;
 import com.elice.jongmin.service.BlogService;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RequiredArgsConstructor
@@ -23,6 +27,18 @@ public class BlogApiController {
     return ResponseEntity.status(HttpStatus.CREATED)
         .body(savedArticle);
   }
+
+  @GetMapping("/api/article")
+  public ResponseEntity<List<ArticleResponse>> findAllArticle(@RequestParam Long id){
+    List<ArticleResponse> articles = blogService.findAll()
+        .stream()
+        .map(ArticleResponse::new)
+        .toList();
+
+    return ResponseEntity.ok()
+        .body(articles);
+  }
+
 
 
 }
--- src/main/java/com/elice/jongmin/service/BlogService.java
+++ src/main/java/com/elice/jongmin/service/BlogService.java
@@ -3,6 +3,7 @@ package com.elice.jongmin.service;
 import com.elice.jongmin.domain.Article;
 import com.elice.jongmin.dto.ArticleRequest;
 import com.elice.jongmin.repository.BlogRepository;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -16,4 +17,14 @@ public class BlogService {
   public Article save(ArticleRequest request) {
     return blogRepository.save(request.toEntity());
   }
+
+  public List<Article> findAll(){
+    return blogRepository.findAll();
+  }
+
+
+  public Article findById(Long id){
+    return blogRepository.findById(id).orElse(null);
+  }
+
 }
