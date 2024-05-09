e016a5da2813e0fafcc632920a66f42cc8bdb407
Jong-Min <codejomo99@gmail.com>
2024-05-09 15:19:21 +0900

    feat : Article Update 기능

--- src/main/java/com/elice/jongmin/controller/BlogApiController.java
+++ src/main/java/com/elice/jongmin/controller/BlogApiController.java
@@ -3,13 +3,18 @@ package com.elice.jongmin.controller;
 import com.elice.jongmin.domain.Article;
 import com.elice.jongmin.dto.ArticleRequest;
 import com.elice.jongmin.dto.ArticleResponse;
+import com.elice.jongmin.dto.UpdateArticleRequest;
 import com.elice.jongmin.service.BlogService;
+import jakarta.websocket.server.PathParam;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -20,6 +25,7 @@ public class BlogApiController {
 
   private final BlogService blogService;
 
+  // POST
   @PostMapping("/api/articles")
   public ResponseEntity<Article> addArticle(@RequestBody ArticleRequest request){
     Article savedArticle = blogService.save(request);
@@ -28,6 +34,7 @@ public class BlogApiController {
         .body(savedArticle);
   }
 
+  // GET
   @GetMapping("/api/articles")
   public ResponseEntity<List<ArticleResponse>> findAllArticle(){
     List<ArticleResponse> articles = blogService.findAll()
@@ -39,6 +46,30 @@ public class BlogApiController {
         .body(articles);
   }
 
+  @GetMapping("/api/article/{id}")
+  public ResponseEntity<ArticleResponse> findByIdArticle(@PathVariable("id") Long id){
+    Article article = blogService.findById(id);
 
+    return ResponseEntity.ok()
+        .body(new ArticleResponse(article));
+  }
+
+  // UPDATE
+  @PutMapping("/api/article/{id}")
+  public ResponseEntity<Article> updateArticle( @PathVariable("id") Long id, @RequestBody UpdateArticleRequest request){
+    Article article = blogService.update(id,request);
+
+    return ResponseEntity.ok()
+        .body(article);
+  }
+
+
+  // DELETE
+  @DeleteMapping("/api/article/{id}")
+  public ResponseEntity<Void> deleteById(@PathVariable("id") Long id){
+    blogService.delete(id);
+
+    return ResponseEntity.ok(null);
+  }
 
 }
--- src/main/java/com/elice/jongmin/service/BlogService.java
+++ src/main/java/com/elice/jongmin/service/BlogService.java
@@ -2,6 +2,7 @@ package com.elice.jongmin.service;
 
 import com.elice.jongmin.domain.Article;
 import com.elice.jongmin.dto.ArticleRequest;
+import com.elice.jongmin.dto.UpdateArticleRequest;
 import com.elice.jongmin.repository.BlogRepository;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
@@ -14,10 +15,13 @@ public class BlogService {
   private final BlogRepository blogRepository;
 
 
+
+  // Create
   public Article save(ArticleRequest request) {
     return blogRepository.save(request.toEntity());
   }
 
+  // Read
   public List<Article> findAll(){
     return blogRepository.findAll();
   }
@@ -25,6 +29,20 @@ public class BlogService {
 
   public Article findById(Long id){
     return blogRepository.findById(id).orElse(null);
+  }
+
+  // Update
+  public Article update(Long id, UpdateArticleRequest request){
+    Article article = blogRepository.findById(id).orElseThrow(()-> new IllegalArgumentException("not found : " + id));
+    article.update(request.getTitle(), request.getContent());
+
+    return article;
+
+  }
+
+  // Delete
+  public void delete(Long id){
+    blogRepository.deleteById(id);
   }
 
 }
